35899f1dad9aa8dd9b91e92414299c59f0be5c45
vahid <vahid.bahramian92@gmail.com>
2021-04-24 15:50:29 -0400

    edit websocket for ubuntu

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from enum import Enum
 import csv
 import matplotlib.pyplot as plt
-from binance.websockets import BinanceSocketManager
+
 from ta import trend
 
 from Connect import Connect
@@ -18,6 +18,7 @@ from BackTest import Algorithm_1
 import Strategy
 from IO import FileWorking
 from threading import Lock
+from binance.websockets import BinanceSocketManager
 
 def main(client):
     """
@@ -36,16 +37,15 @@ def main(client):
     # low_series = pd.Series(candle.low)
     # alg = Algorithm_1(klines, high_series, low_series, candle.close)
     # alg.RunAlgorithm()
-    bsm = BinanceSocketManager(client)
-    bsm.start()
 
+    bsm = BinanceSocketManager(client)
     mutex = Lock()
     candle = Candles(client,mutex)
-    btc_trade = Algo_1(client, bsm , candle, "BTC", "USDT", ignoreLastTrade=True)
+    btc_trade = Algo_1(client, bsm, candle, "BTC", "USDT", ignoreLastTrade=True)
     btc_trade.SetAlgorithmParam(window1=36, window2=48, window3=144, t=18, a=0, b=0.04)
     btc_trade.RunTradeThread()
 
-    eth_trade = Algo_1(client, bsm ,candle, "ETH", "USDT", ignoreLastTrade=True)
+    eth_trade = Algo_1(client, bsm, candle, "ETH", "USDT", ignoreLastTrade=True)
     eth_trade.SetAlgorithmParam(window1=9, window2=24, window3=96, t=26, a=0, b=0.04)
     eth_trade.RunTradeThread()
 
