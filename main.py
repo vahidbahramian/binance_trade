7dbfb349b7aed88368ba1d2736e489cfadee8714
Vahid Bahramian <vahid.bahramian92@gmail.com>
2021-11-07 00:49:53 +0330

    add more currency for online tarde

--- main.py
+++ main.py
@@ -89,77 +89,90 @@ def main(client, currency, param):
         #                                             trade.SetAlgorithmParam(currency[1] + currency[0], p)
         #                                             trade.Run()
         #         trade.LogResult()
+        c = {"BTC": [[date(2017, 8, 1), date(2021, 10, 20)]], "ETH": [[date(2017, 8, 1), date(2021, 10, 20)]],
+             "XRP": [[date(2018, 5, 1), date(2021, 10, 20)]], "TRX": [[date(2018, 6, 1), date(2021, 10, 20)]],
+             "LTC": [[date(2017, 12, 1), date(2021, 10, 20)]], "ADA": [[date(2018, 4, 1), date(2021, 10, 20)]],
+             "BNB": [[date(2017, 11, 1), date(2021, 10, 20)]], "DOGE": [[date(2019, 7, 1), date(2021, 10, 20)]],
+             "XTZ": [[date(2019, 9, 1), date(2021, 10, 20)]], "DOT": [[date(2020, 8, 1), date(2021, 10, 20)]],
+             "MATIC": [[date(2019, 4, 1), date(2021, 10, 20)]], "FTM": [[date(2019, 6, 1), date(2021, 10, 20)]],
+             "ALGO": [[date(2019, 6, 1), date(2021, 10, 20)]]}
 
-        c = {"BNB": [[date(2019, 1, 1), date(2020, 1, 1)], #[date(2018, 1, 1), date(2019, 1, 1)],
-                     [date(2020, 1, 1), date(2021, 1, 1)], [date(2021, 1, 1), date(2021, 10, 1)]],
-             "DOGE": [[date(2019, 8, 1), date(2020, 8, 1)], [date(2020, 8, 1), date(2021, 8, 1)]],
-             "XTZ": [[date(2019, 10, 1), date(2020, 10, 1)], [date(2020, 10, 1), date(2021, 10, 1)]],
-             "DOT": [[date(2020, 9, 1), date(2021, 9, 1)], [date(2021, 3, 1), date(2021, 10, 1)]],
-             "MATIC": [[date(2019, 5, 1), date(2020, 5, 1)], [date(2020, 5, 1), date(2021, 5, 1)],
-                       [date(2021, 1, 1), date(2021, 10, 1)]],
-             "FTM": [[date(2019, 11, 1), date(2020, 11, 1)], [date(2020, 10, 1), date(2021, 10, 1)]],
-             "ALGO": [[date(2019, 7, 1), date(2020, 7, 1)], [date(2020, 7, 1), date(2021, 7, 1)]]}
+        for i, j in c.items():
+            currency = [i, "USDT"]
+            start = j[0][0]
+            stop = j[0][1]
+            trade = BackTest.Algorithm_5(candle, currency, start, stop)
 
-        for c, v in c.items():
-            for i in v:
-                currency = ["BTC", c, "USDT"]
-                start = i[0]
-                stop = i[1]
-                trade = BackTest.Algorithm_5(candle, currency, start, stop)
-                p = {"Win1": 24, "Win2": 48, "Win3": 120, "t": 18, "a": 0, "McGinley_Period": 18, "keltner_Window": 18,
-                     "Multi_ATR": 1.5}
-                trade.SetAlgorithmParam(currency[0] + currency[2], p)
-                if c == "BNB":
-                    p = {"Win1": 9, "Win2": 48, "Win3": 96, "t": 18, "a": 0,
-                         "McGinley_Period": 18, "keltner_Window": 24, "Multi_ATR": 2}
-                elif c == "DOGE":
-                    p = {"Win1": 9, "Win2": 24, "Win3": 72, "t": 48, "a": 0,
-                         "McGinley_Period": 12, "keltner_Window": 18, "Multi_ATR": 2}
-                elif c == "XTZ":
-                    p = {"Win1": 18, "Win2": 24, "Win3": 48, "t": 48, "a": 0,
-                         "McGinley_Period": 24, "keltner_Window": 12, "Multi_ATR": 1.5}
-                elif c == "DOT":
-                    p = {"Win1": 9, "Win2": 24, "Win3": 72, "t": 18, "a": 0,
-                         "McGinley_Period": 18, "keltner_Window": 18, "Multi_ATR": 2}
-                elif c == "MATIC":
-                    p = {"Win1": 24, "Win2": 48, "Win3": 96, "t": 26, "a": 0,
-                         "McGinley_Period": 12, "keltner_Window": 18, "Multi_ATR": 2}
-                elif c == "FTM":
-                    p = {"Win1": 9, "Win2": 24, "Win3": 72, "t": 48, "a": 0,
-                         "McGinley_Period": 18, "keltner_Window": 24, "Multi_ATR": 1.5}
-                elif c == "ALGO":
-                    p = {"Win1": 18, "Win2": 72, "Win3": 96, "t": 18, "a": 0,
-                         "McGinley_Period": 12, "keltner_Window": 12, "Multi_ATR": 2}
-                trade.SetAlgorithmParam(currency[1] + currency[2], p)
-                window1 = [9, 18, 24, 36]
-                window2 = [24, 48, 72]
-                window3 = [48, 72, 96, 120, 144]#[48, 96, 144]
-                t_ = [18, 26, 48]
-                # window1 = [9]
-                # window2 = [24]
-                # window3 = [48]
-                # t_ = [18]
-                McGinley_period = [12, 18, 24, 30]
-                keltner = [12, 18, 24]
-                multi_atr = [1, 1.5, 2]
-                # McGinley_period = [12]
-                # keltner = [12]
-                # multi_atr = [1]
-                for win1 in window1:
-                    for win2 in window2:
-                        if win1 < win2:
-                            for win3 in window3:
-                                if win2 < win3:
-                                    for t in t_:
-                                        print(win1, " ", win2, " ", win3, " ", t, " ")
-                                        for mc_ginley in McGinley_period:
-                                            for k in keltner:
-                                                for atr in multi_atr:
-                                                    p = {"Win1": win1, "Win2": win2, "Win3": win3, "t": t, "a": 0,
-                                                         "McGinley_Period": mc_ginley, "keltner_Window": k, "Multi_ATR": atr}
-                                                    trade.SetAlgorithmParam(currency[1] + currency[0], p)
-                                                    trade.Run()
-                trade.LogResult()
+        # c = {"BNB": [[date(2019, 1, 1), date(2020, 1, 1)], #[date(2018, 1, 1), date(2019, 1, 1)],
+        #              [date(2020, 1, 1), date(2021, 1, 1)], [date(2021, 1, 1), date(2021, 10, 1)]],
+        #      "DOGE": [[date(2019, 8, 1), date(2020, 8, 1)], [date(2020, 8, 1), date(2021, 8, 1)]],
+        #      "XTZ": [[date(2019, 10, 1), date(2020, 10, 1)], [date(2020, 10, 1), date(2021, 10, 1)]],
+        #      "DOT": [[date(2020, 9, 1), date(2021, 9, 1)], [date(2021, 3, 1), date(2021, 10, 1)]],
+        #      "MATIC": [[date(2019, 5, 1), date(2020, 5, 1)], [date(2020, 5, 1), date(2021, 5, 1)],
+        #                [date(2021, 1, 1), date(2021, 10, 1)]],
+        #      "FTM": [[date(2019, 11, 1), date(2020, 11, 1)], [date(2020, 10, 1), date(2021, 10, 1)]],
+        #      "ALGO": [[date(2019, 7, 1), date(2020, 7, 1)], [date(2020, 7, 1), date(2021, 7, 1)]]}
+
+        # for c, v in c.items():
+        #     for i in v:
+        #         currency = ["BTC", c, "USDT"]
+        #         start = i[0]
+        #         stop = i[1]
+        #         trade = BackTest.Algorithm_5(candle, currency, start, stop)
+        #         p = {"Win1": 24, "Win2": 48, "Win3": 120, "t": 18, "a": 0, "McGinley_Period": 18, "keltner_Window": 18,
+        #              "Multi_ATR": 1.5}
+        #         trade.SetAlgorithmParam(currency[0] + currency[2], p)
+        #         if c == "BNB":
+        #             p = {"Win1": 9, "Win2": 48, "Win3": 96, "t": 18, "a": 0,
+        #                  "McGinley_Period": 18, "keltner_Window": 24, "Multi_ATR": 2}
+        #         elif c == "DOGE":
+        #             p = {"Win1": 9, "Win2": 24, "Win3": 72, "t": 48, "a": 0,
+        #                  "McGinley_Period": 12, "keltner_Window": 18, "Multi_ATR": 2}
+        #         elif c == "XTZ":
+        #             p = {"Win1": 18, "Win2": 24, "Win3": 48, "t": 48, "a": 0,
+        #                  "McGinley_Period": 24, "keltner_Window": 12, "Multi_ATR": 1.5}
+        #         elif c == "DOT":
+        #             p = {"Win1": 9, "Win2": 24, "Win3": 72, "t": 18, "a": 0,
+        #                  "McGinley_Period": 18, "keltner_Window": 18, "Multi_ATR": 2}
+        #         elif c == "MATIC":
+        #             p = {"Win1": 24, "Win2": 48, "Win3": 96, "t": 26, "a": 0,
+        #                  "McGinley_Period": 12, "keltner_Window": 18, "Multi_ATR": 2}
+        #         elif c == "FTM":
+        #             p = {"Win1": 9, "Win2": 24, "Win3": 72, "t": 48, "a": 0,
+        #                  "McGinley_Period": 18, "keltner_Window": 24, "Multi_ATR": 1.5}
+        #         elif c == "ALGO":
+        #             p = {"Win1": 18, "Win2": 72, "Win3": 96, "t": 18, "a": 0,
+        #                  "McGinley_Period": 12, "keltner_Window": 12, "Multi_ATR": 2}
+        #         trade.SetAlgorithmParam(currency[1] + currency[2], p)
+        #         window1 = [9, 18, 24, 36]
+        #         window2 = [24, 48, 72]
+        #         window3 = [48, 72, 96, 120, 144]#[48, 96, 144]
+        #         t_ = [18, 26, 48]
+        #         # window1 = [9]
+        #         # window2 = [24]
+        #         # window3 = [48]
+        #         # t_ = [18]
+        #         McGinley_period = [12, 18, 24, 30]
+        #         keltner = [12, 18, 24]
+        #         multi_atr = [1, 1.5, 2]
+        #         # McGinley_period = [12]
+        #         # keltner = [12]
+        #         # multi_atr = [1]
+        #         for win1 in window1:
+        #             for win2 in window2:
+        #                 if win1 < win2:
+        #                     for win3 in window3:
+        #                         if win2 < win3:
+        #                             for t in t_:
+        #                                 print(win1, " ", win2, " ", win3, " ", t, " ")
+        #                                 for mc_ginley in McGinley_period:
+        #                                     for k in keltner:
+        #                                         for atr in multi_atr:
+        #                                             p = {"Win1": win1, "Win2": win2, "Win3": win3, "t": t, "a": 0,
+        #                                                  "McGinley_Period": mc_ginley, "keltner_Window": k, "Multi_ATR": atr}
+        #                                             trade.SetAlgorithmParam(currency[1] + currency[0], p)
+        #                                             trade.Run()
+        #         trade.LogResult()
 
         # currency = ["BTC", "ETH", "XRP", "TRX", "ADA", "LTC", "USDT"]
         # start = date(2019, 1, 1)
